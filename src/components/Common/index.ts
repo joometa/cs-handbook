0b73dec12ffa8b529d981759892301a084e5c167
joometa <jjlee1623@gmail.com>
2022-03-16 23:11:50 +0900

    Feat : Add extra feature

---
+++ src/components/Common/index.ts
@@ -0,0 +1,3 @@
+export { default as GlobalStyle } from './GlobalStyle'
+export { default as Footer } from './Footer'
+export { default as Gnb } from './Gnb'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import React, { FunctionComponent } from 'react'
-import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+import { GlobalStyle, Footer } from '@components/Common'
+import { Introduction } from '@components/Main'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
 
 const IndexPage: FunctionComponent = function () {
   return (
-    <main>
-      <Link to="/infos/">To Infos</Link>
-    </main>
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <Footer />
+    </Container>
   )
 }
 
